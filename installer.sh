0326f15d9195206917da24fe2fea868fa586a1b2
Farsos Bulsara <kingsukhoi@gmail.com>
2017-07-22 14:44:43 -0600

    added programs folder to path and rewrote the installer file to: 1)export resource folder 2)source bashrc mods 3)modify the path

--- installer.sh
+++ installer.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 set -eu
-checkSource (){
-	#http://wiki.bash-hackers.org/howto/conffile#secure_it
-	# check if the file contains something we don't want
-	if egrep -q -v '^#|^[^ ]*=[^;]*' "$1"; then
-		echo "Config file is unclean, cleaning it..." >&2
-		# filter the original to a new file
-		egrep '^#|^[^ ]*=[^;&]*'  "$configfile" > "$configfile_secured"
-		configfile="$configfile_secured"
-	fi
-}
-
-#$1 is the resource folder $2 is the path folder
-addmissingfiles(){
-	missingfiles=$(diff "$1" "$2" | egrep "$1" | cut -d " " -f 4)
-	for prog in $missingfiles; do 
-		ln -s "$termprogloc$prog" "$progpath$prog"
-	done
-}
 
 
-#check and import config fileconfig file 
-configfile='./config/config.cfg'
-configfile_secured='./config/resourceconfig.cfg'
-checkSource $configfile
-source "$configfile"
-#adds missing files
-addmissingfiles $terminalProgramsLocation $installLocation
-#add the resource variable and install if nessary
+#add the resource variable and modify the path
 whereami=`readlink -f "$0"`
 regex="^\/(.+)\/installer.sh$"
 if [[ $whereami =~ $regex ]]; then
@@ -37,8 +12,9 @@ fi
 
 modsString="
 ##mods for resources install start
-export RESOURCEDIR=\"/$whereami/\"
+export RESOURCEDIR=\"/$whereami\"
 source \"\$RESOURCEDIR/TerminalMods/sourcedFiles/bashrcMods\"
+export PATH=\"\$RESOURCEDIR/TerminalMods/programs:\$PATH\"
 ##mods for resources install end
 	"
 echo -e "$modsString" >> ~/.bashrc
