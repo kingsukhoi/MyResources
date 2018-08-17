4ea0a2119cb5418e543928544189c7d64f8a42be
Farsos Bulsara <kingsukhoi@gmail.com>
2018-08-16 21:26:54 -0600

    added amix vim to script

--- installer.sh
+++ installer.sh
@@ -9,6 +9,13 @@ get_email() {
     read email
 }
 
+install_amix_vim() {
+    if [ ! -f ~/.vim_runtime/install_awesome_vimrc.sh ]; then
+        git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+        sh ~/.vim_runtime/install_awesome_vimrc.sh
+        echo "set number" >> ~/.vimrc
+    fi
+}
 
 add_mod_string(){
     modsString="
@@ -66,5 +73,6 @@ main() {
     add_mod_string
     add_trash_folder
     copy_input_rc
+    install_amix_vim
 }
 main "$@"
