d9ce45a9488bbfcedd35a9e05530305caa834349
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2021-11-16 15:01:21 -0700

    Updated vim config

--- installer.sh
+++ installer.sh
@@ -16,7 +16,13 @@ install_amix_vim() {
     if [ ! -f ~/.vim_runtime/install_awesome_vimrc.sh ]; then
         git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
         sh ~/.vim_runtime/install_awesome_vimrc.sh
-        echo "set number" >> ~/.vimrc
+        #echo "set number" >> ~/.vimrc
+        cat >> ~/.vimrc<< EOF
+set tabstop=2
+set expandtab
+set shiftwidth=2
+set number
+EOF
     fi
 }
 
